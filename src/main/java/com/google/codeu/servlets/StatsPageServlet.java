e46ebc2efe4b9036c2886c66626b891e661e21f1
Rabeya Noor <50697269+rnoor98@users.noreply.github.com>
2019-06-08 18:34:56 +0500

    Update StatsPageServlet.java

--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -6,21 +6,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
-/**
- * Responds with a hard-coded message for testing purposes.
- */
-@WebServlet("/stats")
-public class StatsPageServlet extends HttpServlet{
-  
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-   throws IOException {
-  
-  response.getOutputStream().println("hello world");
- }
-
 import com.google.codeu.data.Datastore;
 import com.google.gson.JsonObject;
 
