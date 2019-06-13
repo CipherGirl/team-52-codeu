16b6094de0904cd3f73a2dbe533f751e8d800b75
Rabeya Noor <50697269+rnoor98@users.noreply.github.com>
2019-06-13 15:24:11 +0500

    Update user-page-loader.js

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -77,7 +77,7 @@ function buildMessageDiv(message) {
   const headerDiv = document.createElement('div');
   headerDiv.classList.add('message-header');
   headerDiv.appendChild(document.createTextNode(
-      message.user + ' - ' + new Date(message.timestamp) +"   -"+ message.sscore));
+      message.user + ' - ' + new Date(message.timestamp) +"   -- "+ message.sscore));
 
   const bodyDiv = document.createElement('div');
   bodyDiv.classList.add('message-body');
